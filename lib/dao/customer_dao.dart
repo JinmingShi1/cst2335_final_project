4d453b5fd165d9b248fe551e5af0321d84bcee19
zama0041 <>
2025-07-29 18:11:57 -0400

    created customer_dao

---
+++ lib/dao/customer_dao.dart
@@ -0,0 +1,19 @@
+import 'package:floor/floor.dart';
+
+import '../model/customer.dart';
+
+
+@dao
+abstract class CustomerDao {
+  @Query('SELECT * FROM Customer ORDER BY firstName, lastName')
+  Future<List<Customer>> findAllCustomers();
+
+  @insert
+  Future<int> insertCustomer(Customer customer);
+
+  @update
+  Future<void> updateCustomer(Customer customer);
+
+  @delete
+  Future<void> deleteCustomer(Customer customer);
+}
